a27d87b63a70608f919dd03a6fa8c6b2e20bd11c
EshraghianTechno <esnnnn4@gmail.com>
2023-05-26 20:08:58 +0430

    session18

---
+++ session16/firstSpringWeb/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>launching....</h2>
+</body>
+</html>
---
+++ session18/src/main/java/com/anisa/controller/C02RequestParamController.java
@@ -0,0 +1,24 @@
+package com.anisa.controller;
+
+import com.anisa.entity.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+@RequestMapping("/")
+public class C02RequestParamController {
+
+
+    @GetMapping("/getproductparam")
+    public ModelAndView getProductWithParm(@RequestParam(value = "id") Long id,@RequestParam("name") String name)
+    {
+        Product product=new Product(id,name);
+        System.out.println(product);
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("home1");
+        modelAndView.addObject("product",product);
+        return modelAndView;
+    }
+}
